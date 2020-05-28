83f64e22a38f8596a745bc0db9f5416eee0a3b80
rlatjcj <rlatjcj@naver.com>
2020-05-28 10:05:53 +0900

    [200528] Edit dataloader and augment

--- generator/augment.py
+++ generator/augment.py
@@ -1,57 +1,23 @@
 import numpy as np
-from functools import wraps
-
 import tensorflow as tf
 import tensorflow_addons as tfa
 
-def prep_gate(func):
-    @wraps(func)
-    def _gate(*args):
-        return func(*args)
-    return _gate
 
-class Preprocess:
-    mean_std = [[0.50532048, 0.52774713, 0.49474949],
-                [0.17169899, 0.17590892, 0.18991528]]
+mean_std = {
+    'cub': [[0.48552202, 0.49934904, 0.43224954], 
+            [0.18172876, 0.18109447, 0.19272076]],
+}
 
-    def __init__(self, args, mode, angle_range=15):
+class Augment:    
+    def __init__(self, args, mode):
         self.args = args
         self.mode = mode
-        self.angle_range = angle_range
 
-        self.mean, self.std = self.mean_std
+        self.mean, self.std = mean_std[self.args.dataset]
 
-        self.prep_func_list = [self._standardize]
-        if self.mode == 'train':
-            if self.args.crop:
-                self.prep_func_list.append(self._resizecrop)
-            
-            else:
-                self.prep_func_list.append(self._resize)
-
-            if self.args.angle:
-                self.prep_func_list.append(self._rotate)
-            
-            if self.args.vflip:
-                self.prep_func_list.append(self._verticalflip)
-
-            if self.args.hflip:
-                self.prep_func_list.append(self._horizontalflip)
-            
-            if self.args.distortion:
-                self.prep_func_list.append(self._distortion)
-
-            if self.args.contrast:
-                self.prep_func_list.append(self._contrast)
-
-            if self.args.noise:
-                self.prep_func_list.append(self._noise)
-
-        else:
-            self.prep_func_list.append(self._resize)
-
-    @prep_gate
-    def _standardize(self, x, y, data):
+    def _standardize(self, x):
+        x = tf.cast(x, tf.float32)
+        x /= 255.
         if self.args.standardize == 'minmax1':
             pass
         elif self.args.standardize == 'minmax2':
@@ -63,85 +29,131 @@ class Preprocess:
         elif self.args.standardize == 'eachnorm':
             x = (x-tf.math.reduce_mean(x))/tf.math.reduce_std(x)
         else:
-            raise
-        
-        return x, y, data
+            raise ValueError()
 
-    @prep_gate
-    def _crop(self, x, y, data):
-        if tf.math.reduce_sum(data) > 0:
-            x = tf.image.random_crop(x, data)
-        return x, y, data
+        return x
 
-    @prep_gate
-    def _resize(self, x, y, data):
-        img_min, img_max = tf.math.reduce_min(x), tf.math.reduce_max(x)
-        x = tf.image.resize(x, (self.args.img_size, self.args.img_size))
-        x = tf.clip_by_value(x, img_min, img_max)
-        return x, y, data
+    ## need img shape ##
+    def _pad(self, x, shape):
+        length = tf.reduce_max(shape)
+        paddings = [
+            [(length-shape[0])//2, length-((length-shape[0])//2+shape[0])], 
+            [(length-shape[1])//2, length-((length-shape[1])//2+shape[1])], [0, 0]]
+        x = tf.pad(x, paddings, 'CONSTANT', constant_values=0)
+        return x
 
-    @prep_gate
-    def _resizecrop(self, x, y, data):
-        img_min, img_max = tf.math.reduce_min(x), tf.math.reduce_max(x)
+    def _random_crop(self, x, shape):
+        begin, size, bboxes = tf.image.sample_distorted_bounding_box(
+            shape, [[[0, 0, 1, 1]]], min_object_covered=.5)
+        x = tf.slice(x, begin, size)
+        return x
+
+    def _center_crop(self, x, shape):
+        length = tf.reduce_min(shape[:2])
+        x = tf.image.crop_to_bounding_box(x, (shape[0]-length)//2, (shape[1]-length)//2, length, length)
+        return x
+    
+    ####################
+
+    def _resize(self, x):
+        x = tf.image.resize(x, (self.args.img_size, self.args.img_size), tf.image.ResizeMethod.BICUBIC)
+        x = tf.saturate_cast(x, tf.uint8)
+        return x
+
+    def _resize_crop_pad(self, x):
         x = tf.image.resize_with_crop_or_pad(x, self.args.img_size, self.args.img_size)
-        x = tf.clip_by_value(x, img_min, img_max)
-        return x, y, data
+        return x
 
-    @prep_gate
-    def _rotate(self, x, y, data):
-        img_min, img_max = tf.math.reduce_min(x), tf.math.reduce_max(x)
+    def _rotate(self, x):
         x = tfa.image.rotate(x, tf.random.uniform(minval=-self.args.angle, maxval=self.args.angle, shape=[])*np.pi/180, interpolation='BILINEAR')
-        x = tf.clip_by_value(x, img_min, img_max)
-        return x, y, data
+        x = tf.saturate_cast(x, tf.uint8)
+        return x
 
-    @prep_gate
-    def _noise(self, x, y, data):
-        img_min, img_max = tf.math.reduce_min(x), tf.math.reduce_max(x)
-        x += tf.random.normal(stddev=.1, shape=(self.args.img_size, self.args.img_size, self.args.img_channel))
-        x = tf.clip_by_value(x, img_min, img_max)
-        return x, y, data
+    def _noise(self, x):
+        x = tf.cast(x, tf.float32)
+        x += tf.random.normal(
+            stddev=self.args.noise, 
+            shape=(self.args.img_size, self.args.img_size, self.args.img_channel)) * \
+                tf.constant(127, tf.float32)
+        x = tf.saturate_cast(x, tf.uint8)
+        return x
 
-    @prep_gate
-    def _contrast(self, x, y, data):
-        img_min, img_max = tf.math.reduce_min(x), tf.math.reduce_max(x)
-        # return tf.image.random_contrast(x, .5, 1.5)
-        x *= tf.random.uniform(minval=.9, maxval=1.1, shape=(self.args.img_size, self.args.img_size, self.args.img_channel))
-        x = tf.clip_by_value(x, img_min, img_max)
-        return x, y, data
+    def _contrast(self, x):
+        x = tf.image.random_contrast(x, lower=1-self.args.contrast, upper=1+self.args.contrast)
+        x = tf.saturate_cast(x, tf.uint8)
+        return x
 
-    @prep_gate
-    def _distortion(self, x, y, data, s=.5):
-        img_min, img_max = tf.math.reduce_min(x), tf.math.reduce_max(x)
-        def __random_apply(func, x, p):
-            return tf.cond(
-                tf.less(tf.random.uniform([], minval=0, maxval=1, dtype=tf.float32),
-                        tf.cast(p, tf.float32)),
-                lambda: func(x),
-                lambda: x)
+    def _brightness(self, x):
+        x = tf.image.random_brightness(x, max_delta=self.args.brightness)
+        x = tf.saturate_cast(x, tf.uint8)
+        return x
 
-        def __color_jitter(x):
-            x = tf.image.random_brightness(x, max_delta=0.8*s)
-            x = tf.image.random_contrast(x, lower=1-0.8*s, upper=1+0.8*s)
-            x = tf.image.random_saturation(x, lower=1-0.8*s, upper=1+0.8*s)
-            x = tf.image.random_hue(x, max_delta=0.2*s)
-            x = tf.clip_by_value(x, img_min, img_max)
-            return x
+    def _saturation(self, x):
+        x = tf.image.random_saturation(x, lower=1-self.args.contrast, upper=1+self.args.contrast)
+        x = tf.saturate_cast(x, tf.uint8)
+        return x
 
-        def __color_drop(x):
+    def _hue(self, x):
+        x = tf.image.random_hue(x, max_delta=self.args.hue)
+        x = tf.saturate_cast(x, tf.uint8)
+        return x
+
+    def _gray(self, x):
+        if tf.random.uniform([]) < .2:
             x = tf.image.rgb_to_grayscale(x)
             x = tf.tile(x, [1, 1, 3])
-            return x
+        return x
 
-        x = __random_apply(__color_jitter, x, p=.8)
-        x = __random_apply(__color_drop, x, p=.2)
-        return x, y, data
-
-    @prep_gate
-    def _verticalflip(self, x, y, data):
+    def _verticalflip(self, x):
         x = tf.image.random_flip_up_down(x)
-        return x, y, data
+        return x
 
-    @prep_gate
-    def _horizontalflip(self, x, y, data):
+    def _horizontalflip(self, x):
         x = tf.image.random_flip_left_right(x)
-        return x, y, data
+        return x
+
+
+class WeakAugment(Augment):
+    def __init__(self, args, mode):
+        super().__init__(args, mode)
+        self.augment_list = []
+        if self.mode == 'train':
+            if self.args.crop:
+                self.augment_list.append(self._random_crop)
+            else:
+                self.augment_list.append(self._pad)
+            
+            self.augment_list.append(self._resize)
+            
+            if self.args.angle:
+                self.augment_list.append(self._rotate)
+            
+            if self.args.vflip:
+                self.augment_list.append(self._verticalflip)
+
+            if self.args.hflip:
+                self.augment_list.append(self._horizontalflip)
+
+            if self.args.brightness:
+                self.augment_list.append(self._brightness)
+
+            if self.args.contrast:
+                self.augment_list.append(self._contrast)
+
+            if self.args.saturation:
+                self.augment_list.append(self._saturation)
+
+            if self.args.hue:
+                self.augment_list.append(self._hue)
+
+            if self.args.gray:
+                self.augment_list.append(self._gray)
+
+            if self.args.noise:
+                self.augment_list.append(self._noise)
+
+        else:
+            self.augment_list.append(self._center_crop)
+            self.augment_list.append(self._resize)
+
+        self.augment_list.append(self._standardize)
--- generator/dataloader.py
+++ generator/dataloader.py
@@ -1,224 +1,65 @@
 import os
-import cv2
-import json
-import threading
 import numpy as np
 import pandas as pd
 
 import tensorflow as tf
 
-
-class threadsafe_iter:
-    def __init__(self, it):
-        self.it = it
-        self.lock = threading.Lock()
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        with self.lock:
-            return next(self.it)
-
-def threadsafe_generator(f):
-    def g(*a, **kw):
-        return threadsafe_iter(f(*a, **kw))
-    return g
-
-
-def dataloader(args, mode, datalist, shuffle=True):
-    def _get_pad(_img):
-        height, width = _img.shape[:2]
-        if height > width:
-            residual = (height - width)//2
-            pad_width = [[0, 0], [residual, height-(width+residual)], [0, 0]]
-            
-        elif width > height:
-            residual = (width - height)//2
-            pad_width = [[residual, width-(height+residual)], [0, 0], [0, 0]]
-        
-        else:
-            pad_width = [[0, 0], [0, 0], [0, 0]]
-        
-        _img_pad = np.pad(_img, pad_width)
-        return _img_pad
-
-    def _set_cropbox(_img):
-        crop_ratio = np.random.uniform(low=.8, high=1., size=(2,))
-        crop_size = [int(s*r) for s, r in zip(_img.shape[:2], crop_ratio)]+[3]
-        # aspect_ratio = np.random.uniform(low=3/4, high=4/3)
-        # ratio = [crop_ratio, crop_ratio*aspect_ratio]
-        # crop_size = [np.minimum(52, int(52*r)) for r in ratio]+[3]
-        return crop_size
-
-    def _loadimg(_data, _mode):
-        img = cv2.imread(os.path.join(args.data_path, _data[0]))[...,::-1]
-        label = np.array(_data[1:], dtype=np.float32)
-        if _mode == 'train':
-            prob = np.random.random()
-            if args.crop == 0:
-                crop_size = [0, 0, 0]
-            elif args.crop == 1:
-                if prob > .5:
-                    img = _get_pad(img)
-                crop_size = [0, 0, 0]
-            elif args.crop == 2:
-                if prob < .3:
-                    crop_size = [0, 0, 0]
-                elif .3 <= prob < .6:
-                    img = _get_pad(img)
-                    crop_size = [0, 0, 0]
-                else:
-                    crop_size = _set_cropbox(img)
-                
-        else:
-            img = _get_pad(img)
-            crop_size = [0, 0, 0]
-
-        img = img.astype(np.float32)
-        img /= 255.
-        return img, label, crop_size
-
-    def _class_gen(_datalist, flip_class=None):
-        indices = np.arange(len(_datalist))
-        # print(_datalist.shape)
-        while True:
-            if shuffle:
-                indices = np.random.permutation(len(_datalist))
-
-            for idx in indices:
-                data = _datalist[idx]
-                img, label, crop_bbox = _loadimg(data, mode)
-                yield img, label, crop_bbox
-
-    @threadsafe_generator
-    def _loader():
-        if mode == 'train':
-            class_gen = {c: _class_gen(datalist[np.where(datalist[:,c+1] == 1)]) for c in range(args.classes)}
-        else:
-            indices = np.arange(len(datalist))
-        
-        while True:
-            if mode == 'train':
-                if shuffle:
-                    class_list = np.random.permutation(args.classes)
-                else:
-                    class_list = np.arange(args.classes)
-
-                for c in range(len(class_list)):
-                    if args.mixup:
-                        if np.random.random() > .5:
-                            x1, y1, _ = next(class_gen[class_list[c]])
-                            x2, y2, _ = next(class_gen[class_list[0 if c == len(class_list)-1 else c+1]])
-
-                            height = min(x1.shape[0], x2.shape[0])
-                            width = min(x1.shape[1], x2.shape[1])
-                            x1 = x1[max(0, (x1.shape[0]-height)//2):min(x1.shape[0], (x1.shape[0]+height)//2),
-                                    max(0, (x1.shape[1]-width)//2):min(x1.shape[1], (x1.shape[1]+width)//2)]
-                            x2 = x2[max(0, (x2.shape[0]-height)//2):min(x2.shape[0], (x2.shape[0]+height)//2),
-                                    max(0, (x2.shape[1]-width)//2):min(x2.shape[1], (x2.shape[1]+width)//2)]
-                            
-                            ratio = np.random.beta(a=.2, b=.2)
-                            ratio = np.maximum(.5, ratio)
-                            ratio = np.clip(ratio, 1e-7, 1-1e-7)
-                            x = x1*ratio + x2*(1-ratio)
-                            x = np.clip(x, 0., 1.)
-                            y = y1*ratio + y2*(1-ratio)
-                            crop_bbox = _set_cropbox(x)
-                            yield x, y, crop_bbox
-                        else:
-                            yield next(class_gen[class_list[c]])
-                    else:
-                        yield next(class_gen[class_list[c]])
-            else:
-                for idx in indices:
-                    data = datalist[idx]
-                    img, label, crop_bbox = _loadimg(data, mode)
-                    if mode == 'test':
-                        yield img, label, [data[0]]
-                    else:
-                        yield img, label, crop_bbox
-
-    return _loader
+from generator.augment import WeakAugment
 
 
 def set_dataset(args, logger):
-    trainset_whole = []
-    for v in range(5):
-        if v+1 == args.version:
-            continue
-        trainset_whole += pd.read_csv(os.path.join(args.src_path, 'trainset_{}.csv'.format(v+1))).values.tolist()
-    valset_whole = pd.read_csv(os.path.join(args.src_path, 'trainset_{}.csv'.format(args.version))).values.tolist()
-    
-    trainset = []
-    valset = []
-    for dataset in ['trainset', 'valset']:
-        exec('{} = []'.format(dataset))
-        for t in eval('{}_whole'.format(dataset)):
-            label = t[1:]
-            if sum(label) == 1:
-                eval(dataset).append(t)
+    trainset = pd.read_csv(os.path.join(args.src_path, '{}_trainset.csv'.format(args.dataset))).values.tolist()
+    valset = pd.read_csv(os.path.join(args.src_path, '{}_valset.csv'.format(args.dataset))).values.tolist()
+    testset = pd.read_csv(os.path.join(args.src_path, '{}_testset.csv'.format(args.dataset))).values.tolist()
+    return np.array(trainset, dtype='object'), np.array(valset, dtype='object'), np.array(testset, dtype='object')
+
+
+def dataloader(args, datalist, mode, shuffle=True):
+    imglist, labellist = datalist[:,0].tolist(), datalist[:,1].tolist()
+    imglist = [os.path.join(args.data_path, i) for i in imglist]
+    dataset = tf.data.Dataset.from_tensor_slices((imglist, labellist))
+    dataset = dataset.repeat()
+    if shuffle:
+        dataset = dataset.shuffle(len(datalist))
+
+    def fetch_dataset(path, y):
+        x = tf.io.read_file(path)
+        return tf.data.Dataset.from_tensors((x, y))
+
+    dataset = dataset.interleave(
+        fetch_dataset, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+    def augmentation(img, label, shape):
+        if args.augment == 'weak':
+            augment = WeakAugment(args, mode)
+        else:
+            raise ValueError()
+
+        for f in augment.augment_list:
+            if 'crop' in f.__name__ or 'pad' in f.__name__:
+                img = f(img, shape)
             else:
-                logger.info('{} don\'t have label!'.format(t[0]))
+                img = f(img)
 
-    logger.info('Train set : {}'.format(len(trainset)))
-    logger.info('Validation set : {}'.format(len(valset)))
+        label = tf.one_hot(label-1, args.classes)
+        return img, label
 
-    if args.mode == 'supervised':
-        return np.array(trainset, dtype='object'), np.array(valset, dtype='object')
-        
-    elif args.mode == 'uda':
-        unlabelset = pd.read_csv(os.path.join(args.src_path, 'u_train_in.csv')).values.tolist()
-        logger.info('Unlabeled set : {}'.format(len(unlabelset)))
-        return np.array(trainset, dtype='object'), np.array(valset, dtype='object'), np.array(unlabelset, dtype='object')
-
-
-def create_generator(args, dataset, mode, batch_size):
-    output_types=(tf.dtypes.float32, tf.dtypes.float32)
-    output_shapes = (tf.TensorShape((None, None, None,)), tf.TensorShape((None,)))
-
-    if mode == 'test':
-        output_types += (tf.dtypes.string,)
-        output_shapes += (tf.TensorShape((None,)),)
-    else:
-        output_types += (tf.dtypes.int32,)
-        output_shapes += (tf.TensorShape((None,)),)
-        
-    from .augment import Preprocess
-    prep_func = Preprocess(args, mode)
-
-    generator = tf.data.Dataset.from_generator(
-        dataloader(args, mode, dataset, shuffle=True if mode == 'train' else False), 
-        output_types=output_types,
-        output_shapes=output_shapes,
-    )
-    for f in prep_func.prep_func_list:
-        generator = generator.map(lambda *args: f(*args), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    
-    if mode != 'test':
-        if args.embedding == 'arcface':
-            generator = generator.map(lambda x, y, z: ({'main_input': x, 'arcface_input': y}, 
-                                                       {'arcface_output': y}), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    def preprocess_image(img, label):
+        shape = tf.image.extract_jpeg_shape(img)
+        img = tf.io.decode_jpeg(img, channels=3)
+        img, label = augmentation(img, label, shape)
+        if args.embedding == 'softmax':
+            tensors = ({'main_input': img}, {'main_output': label})
         elif args.embedding == 'dual':
-            generator = generator.map(lambda x, y, z: ({'main_input': x, 'arcface_input': y}, 
-                                                       {'main_output': y, 'arcface_output': y}), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-        else:
-            generator = generator.map(lambda x, y, z: ({'main_input': x}, {'main_output': y}), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+            tensors = ({'main_input': img, 'arcface_input': label}, 
+                       {'main_output': label, 'arcface_output': label})
+        elif args.embedding == 'arcface':
+            tensors = ({'main_input': img, 'arcface_input': label}, 
+                       {'arcface_output': label})
+        return tensors
 
-    else:
-        if args.embedding == 'arcface':
-            generator = generator.map(lambda x, y, z: ({'main_input': x, 'arcface_input': y}, 
-                                                       {'arcface_output': y},
-                                                       {'patient': z}), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-        elif args.embedding == 'dual':
-            generator = generator.map(lambda x, y, z: ({'main_input': x, 'arcface_input': y}, 
-                                                       {'main_output': y, 'arcface_output': y},
-                                                       {'patient': z}), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-        else:
-            generator = generator.map(lambda x, y, z: ({'main_input': x}, {'main_output': y},
-                                                       {'patient': z}), num_parallel_calls=tf.data.experimental.AUTOTUNE)
-
-    generator = generator.batch(batch_size)
-    generator = generator.prefetch(tf.data.experimental.AUTOTUNE)
-
-    return generator
+    dataset = dataset.map(
+        preprocess_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    dataset = dataset.batch(args.batch_size)
+    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    return dataset
